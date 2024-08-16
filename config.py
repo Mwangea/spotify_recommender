4c3ada9d57be7784f9a3b1500b31a3d2eab500df
mwangea <mwangeamusa@gmail.com>
2024-08-16 17:33:49 +0300

    updated

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 #spotify API  Credentials
-CLIENT_ID = '07cc1f8c00ed4eeda4d94cbdb24bc268'
-CLIENT_SECRET = 'cce0763da418425ea6cafdb1cb448664'
+CLIENT_ID = '07cc1f8c00ed4eeda4d94cbdb24bc268' #your_client_id
+CLIENT_SECRET = 'cce0763da418425ea6cafdb1cb448664' #your_client_secret
 REDIRECT_URI = 'http://localhost:8888/callback'  # This should match the redirect URI in your Spotify Developer Dashboard
 
 # Recommendation settings
